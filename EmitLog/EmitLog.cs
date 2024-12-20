0ecce4f040f6fe654338ee09a54436d3c8fcd930
Pat Szemora <p.charles.moran@gmail.com>
2024-12-20 12:12:16 -0500

    Tutorial 3: Exchanges
    Details:
    - A queue gets bound to an exchange.
    - This is done by specifying exhange and queue arguments in ExchangeDeclareAsync() and QueueBindAsync().
    - See code and tutorial for more details.
    - The steps are explicit and important.

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+const string _hostName = "localhost";
+const string _exchange = "logs";
+
+// 1. Create = Establish Connection
+var factory = new ConnectionFactory { HostName = _hostName };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+// 2. Declare Exchange
+// Exchange
+// = on one side, an exchange receives messages from producers; on the other side, the exchange pushes those messages to queues
+// The exchange must know exactly what to do with a messages it receives
+// e.g. append the message to a particular queue? Append the message to many queues? Discard the message?
+// In previous tutorials we covered these concepts:
+// a. producer => is a user application that sends messages.
+// b. queue => is a buffer that stores messages.
+// c. consumer => is a user application that receives messages.
+// - An exchange sits between producer and queue(s)
+// - "The core idea in the messaging model in RabbitMQ is that the producer never sends any messages directly to a queue. Actually, quite often the producer doesn't even know if a message will be delivered to any queue at all."
+// `ExchangeType.Fanout`
+// = broadcasts all the messages it receives to all the queues it knows.
+// When `channel.ExchangeDeclareAsync(exchnage: string.Empty, ...)`, the 'default' or nameless exchange is used to declare it.
+// You can view the default exchange in the RabbitMQ mgmt UI on the Exchanges page: "(AMQP default)"
+await channel.ExchangeDeclareAsync(exchange: _exchange, type: ExchangeType.Fanout);
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+// 3. Publish Message to Exchange
+// When await channel.BasicPublishAsync(exchange: <name_of_exchange>, routingKey: <name_of_queue>, ...), messages are 'published to the name_of_exchange, and routed to the name_of_queue if it exists.
+// Important Detail1 : We need to supply a routingKey when sending, but its value is ignored for fanout exchanges.
+// - We declare a `fanout` exchange above: await channel.ExchangeDeclareAsync(exchange: _exchange, type: ExchangeType.Fanout);
+// Important Detail 2: The messages will be lost if no queue is bound to the exchange yet, but that's okay for us; if no consumer is listening yet we can safely discard the message.
+// - We bind the server-named queue to the "logs" exchange in `ReceiveLogs.cs`
+await channel.BasicPublishAsync(
+    exchange: _exchange, 
+    routingKey: string.Empty,
+    body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+}
---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Tasks;
+
+const string _hostName = "localHost";
+const string _exchange = "logs";
+
+// 1. Create + Open Connection to Channel
+var factory = new ConnectionFactory { HostName = _hostName };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+// 2. Declare Exchange
+// Important Detail 2: The messages will be lost if no queue is bound to the exchange yet, but that's okay for us; if no consumer is listening yet we can safely discard the message.
+// - We bind the server-named queue to the "logs" exchange below
+await channel.ExchangeDeclareAsync(exchange: _exchange, type: ExchangeType.Fanout);
+
+// 3. Declare a Server-Named Queue
+// In the .NET client, when we supply no parameters to QueueDeclareAsync() we create a non-durable, exclusive, autodelete queue with a generated name (e.g. amq.gen-JzTY20BRgKO-HjmUJj0wLg)
+QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync();
+string queueName = queueDeclareResult.QueueName;
+
+// 4. Create "Binding"
+// Binding is the relationship between an exchange and queue
+// e.g.
+// Exchange => logs
+// is bound to
+// Queue => amq.gen-JzTY20BRgKO-HjmUJj0wLg
+// The logs exchange will append messages to the amq.gen-JzTY20BRgKO-HjmUJj0wLg queue
+// You can view Bindings in the RabbitMQ Mgmt UI or Console.
+// This is similar to viewing the Exchanges
+// RabbitMQ Mgmt UI
+// http://localhost:15672 -> Exchanges -> Bindings
+// RabbitMQ Console
+// 1. docker exec -it rabbitmq bash (Assuming running RabbitMQ Server in a Docker Container)
+// 2. rabbitmqctl list_bindings
+await channel.QueueBindAsync(queue: queueName, exchange: _exchange, routingKey: string.Empty);
+
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) => 
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] {message}");
+    return Task.CompletedTask;
+};
+
+// Note from Module 02: Work Queues
+// autoAck: true turns off Manual message acknowledgement (default)
+// We typically always want autoAck : false
+// The reason why is b/c RabbitMQ will 're-queue' messages that are not processed (i.e. messages that a consumer does not send a delivery message acknowledgement)
+// This is helpful in scenarios where there are network failures and/or Consumer failures (e.g. A Worker fails or is taken off line)
+// RabbitMQ keeps track of the messages it sends for this purpose.
+// We'll implement an actual message acknowledgement in Module 2
+await channel.BasicConsumeAsync(
+    queue: queueName, 
+    autoAck: true, 
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
